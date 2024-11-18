9a511c40a2caac290023a4c6f7c63585444e19cb
=ahalim260187 <raffahaitami@gmail.com>
2024-11-19 04:38:40 +0800

    Admin Forgot and Reset Password

--- app/Http/Controllers/Admin/Auth/NewPasswordController.php
+++ app/Http/Controllers/Admin/Auth/NewPasswordController.php
@@ -38,7 +38,7 @@ class NewPasswordController extends Controller
         // Here we will attempt to reset the user's password. If it is successful we
         // will update the password on an actual user model and persist it to the
         // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
+        $status = Password::broker('admins')->reset(
             $request->only(
                 'email',
                 'password',
@@ -62,7 +62,7 @@ class NewPasswordController extends Controller
         // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
             ? redirect()
-                ->route('login')
+                ->route('admin.login')
                 ->with('status', __($status))
             : back()
                 ->withInput($request->only('email'))
