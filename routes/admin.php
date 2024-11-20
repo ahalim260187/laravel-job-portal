f58882ebfed59f73205a29436074f78a0bb83dd0
ahalim260187 <raffahaitami@gmail.com>
2024-11-20 11:44:27 +0800

    Admin Dashboard Template - Creating Master Layout

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(
@@ -46,9 +47,8 @@ Route::group(
     ['middleware' => ['auth:admin'], 'prefix' => 'admin', 'as' => 'admin.'],
     function () {
         // admin dashboard
-        Route::get('dashboard', function () {
-            return view('admin.dashboard.index');
-        })->name('dashboard');
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
         Route::get(
             'verify-email',
             EmailVerificationPromptController::class
