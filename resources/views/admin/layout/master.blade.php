1eeb4aa2e39a90c70f2c46e754fae2efa54baff0
ahalim260187 <raffahaitami@gmail.com>
2024-11-20 14:37:02 +0800

    Admin Dashboard Template - Cleaning Master, Navbar and Sidebar

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -34,8 +34,8 @@
             </div>
             <footer class="main-footer">
                 <div class="footer-left">
-                    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
-                        Nauval Azhar</a>
+                    Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a
+                        href="https://www.google.com">My Company Later</a>
                 </div>
                 <div class="footer-right">
 
