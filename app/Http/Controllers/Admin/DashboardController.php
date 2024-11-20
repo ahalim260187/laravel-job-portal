01f294518a4a91db66f2e9d9968ed7374aa9b691
ahalim260187 <raffahaitami@gmail.com>
2024-11-20 13:56:03 +0800

    Admin Dashboard Template - Create Sidebar, Navbar and include them into master layout file using @yield,@include and @section

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,6 +10,6 @@ class DashboardController extends Controller
 {
     public function index(): View
     {
-        return view('admin.layout.master');
+        return view('admin.dashboard.index');
     }
 }
