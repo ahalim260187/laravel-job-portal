90847cc4d79fe5ba219852510eb143b6910d3d82
ahalim260187 <raffahaitami@gmail.com>
2024-11-22 16:15:36 +0800

    Admin Dashboard Template - Adding Reset Password Page

--- resources/views/admin/auth/reset-password.blade.php
+++ resources/views/admin/auth/reset-password.blade.php
@@ -1,41 +1,67 @@
-<x-guest-layout>
-    <h1 style="color: red">Admin Form Reset Password</h1>
-    <form method="POST" action="{{ route('admin.password.store') }}">
-        @csrf
+@extends('admin.auth.layout.auth-master')
+@section('content')
+    <section class="section">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                    <div class="login-brand">
+                        <img src="{{ asset('admin/assets/img/stisla-fill.svg') }}" alt="logo" width="100"
+                            class="shadow-light rounded-circle">
+                    </div>
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                    <div class="card card-primary">
+                        <div class="card-header d-flex justify-content-center">
+                            <h4>Reset Password</h4>
+                        </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required
-                autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('admin.password.store') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <!-- Password Reset Token -->
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input id="email" type="email"
+                                        class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email"
+                                        tabindex="1" value="{{ old('email', $request->email) }}" required autofocus>
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password">New Password</label>
+                                    <input id="password" type="password"
+                                        class="form-control pwstrength {{ $errors->has('password') ? 'is-invalid' : '' }}"
+                                        data-indicator="pwindicator" name="password" tabindex="2" required>
+                                    <div id="pwindicator" class="pwindicator">
+                                    </div>
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password_confirmation" :value="__('Confirm Password')">Confirm
+                                        Password</label>
+                                    <input id="password_confirmation" type="password"
+                                        class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}"
+                                        name="password_confirmation" tabindex="2" required>
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                        Reset Password
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="simple-footer">
+                        Copyright &copy; Web Laravel Job Portal
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+@endsection
