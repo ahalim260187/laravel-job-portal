85e94943b8e2ff0bc5e206afaad664f86f1e4c3b
ahalim260187 <raffahaitami@gmail.com>
2024-12-04 15:48:42 +0800

    Company Info - Implementing Sluggable ke database pakai package cviebrock/eloquent-sluggable

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,18 +2,29 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
+    use Sluggable;
     use HasFactory;
     protected $fillable = [
         'user_id',
+        'slug',
         'logo',
         'banner',
         'name',
         'bio',
         'vision',
     ];
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+            ]
+        ];
+    }
 }
