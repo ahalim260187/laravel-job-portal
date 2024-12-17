de02303734ada8003b1a85511b1d6b9b1297e08e
ahalim260187 <raffahaitami@gmail.com>
2024-12-17 12:59:29 +0800

    Admin Side -- Create Template for showing all data using table bootstraps dan paginate

---
+++ resources/views/admin/industry-type/index.blade.php
@@ -0,0 +1,68 @@
+@extends('admin.layout.master')
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Industry Types</h1>
+        </div>
+        <div class="section-body">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header ">
+                        <h4>All Industry Types</h4>
+                        <div class="card-header-form">
+                            <form>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-primary" style="height: 40px"><i
+                                                class="fas fa-search"></i></button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <a href="" class="btn btn-sm btn-primary"><i class="fas fa-plus-circle"></i> Create New</a>
+                    </div>
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Slug</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                    <td>Create a mobile app</td>
+                                    <td>
+                                        create-a-mobile-app
+                                    </td>
+                                    <td>
+                                        <div class="badge badge-success">Completed</div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer text-right">
+                <nav class="d-inline-block">
+                    <ul class="pagination mb-0">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1"><i class="fas fa-chevron-left"></i></a>
+                        </li>
+                        <li class="page-item active"><a class="page-link" href="#">1 <span
+                                    class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">2</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </section>
+@endsection
