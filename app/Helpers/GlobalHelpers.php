9490cbd98ba1cecd249cb9fe2ff6675028fe3891
=ahalim260187 <raffahaitami@gmail.com>
2024-12-26 14:26:38 +0800

    Creating Admin Sidebar Active dinamic global function

--- app/Helpers/GlobalHelpers.php
+++ app/Helpers/GlobalHelpers.php
@@ -2,8 +2,30 @@
 
 // Check input error
 if (!function_exists('hasError')) {
-    function hasError($errors, string $name): ?String
+    function hasError($errors, string $name): ?string
     {
         return $errors->has($name) ? 'is-invalid' : '';
+    }
+}
+
+// Set Sidebar active
+// cara1
+// if (!function_exists('setSidebarActive')) {
+//     function setSidebarActive(array $routes): string
+//     {
+//         return in_array(Route::currentRouteName(), $path) ? 'active' : '';
+//     }
+// }
+
+// cara2
+if (!function_exists('setSidebarActive')) {
+    function setSidebarActive(array $routes): string
+    {
+        foreach ($routes as $route) {
+            if (request()->routeIs($route)) {
+                return 'active';
+            }
+        }
+        return '';
     }
 }
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -8,31 +8,34 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="active">
+            <li class="{{ setSidebarActive(['admin.dashboard']) }}">
                 <a href="{{ route('admin.dashboard') }}" class="nav-link"><i
                         class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Starter</li>
-            <li class="dropdown">
+            <li class="dropdown {{ setSidebarActive(['admin.industry-type.*', 'admin.organization-type.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
-                    <span>Layout</span></a>
+                    <span>Profile Info</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{ route('admin.industry-type.index') }}">Industry Type</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.organization-type.index') }}">Organization Type</a>
+                    <li class="{{ setSidebarActive(['admin.industry-type.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.industry-type.index') }}">Industry Type</a></li>
+                    <li class="{{ setSidebarActive(['admin.organization-type.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.organization-type.index') }}">Organization Type</a>
                     </li>
                 </ul>
             </li>
-            <li class="dropdown">
+            <li class="dropdown {{ setSidebarActive(['admin.countries.*', 'admin.states.*', 'admin.cities.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-globe"></i>
                     <span>Location</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{ route('admin.countries.index') }}">Countries</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.states.index') }}">States</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.cities.index') }}">Cities</a></li>
+                    <li class="{{ setSidebarActive(['admin.countries.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.countries.index') }}">Countries</a></li>
+                    <li class="{{ setSidebarActive(['admin.states.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.states.index') }}">States</a></li>
+                    <li class="{{ setSidebarActive(['admin.cities.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.cities.index') }}">Cities</a></li>
             </li>
         </ul>
-        </li>
-        <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
-        </ul>
+        {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
     </aside>
 </div>
